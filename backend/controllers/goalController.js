934137572a8e8354b038afdefc0262441020f4af
Ignacio Cunial <igsecu@hotmail.com>
2024-04-22 20:42:38 -0300

    Updated controllers -> used database

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,6 +1,9 @@
+const Goal = require("../models/goalModel");
+
 // Get goals
 const getGoals = async (req, res, next) => {
-  res.status(200).json({ msg: "Get goals!" });
+  const goals = await Goal.find();
+  res.status(200).json({ data: goals });
 };
 
 // Create goal
@@ -13,17 +16,48 @@ const createGoal = async (req, res, next) => {
     });
   }
 
-  res.status(200).json({ msg: "Set goal!" });
+  const goal = await Goal.create({
+    text,
+  });
+
+  res.status(200).json({ msg: "Goal created successfully!", data: goal });
 };
 
 // Update goal
 const updateGoal = async (req, res, next) => {
-  res.status(200).json({ msg: "Update goal!" });
+  const { id } = req.params;
+  const { text } = req.body;
+
+  const goal = await Goal.findById(id);
+
+  if (!goal) {
+    return res.status(404).json({
+      msg: `Goal with ID: ${id} not found!`,
+    });
+  }
+
+  const updatedGoal = await Goal.findByIdAndUpdate(id, text, { new: true });
+
+  res
+    .status(200)
+    .json({ msg: "Goal Updated Successfully!", data: updatedGoal });
 };
 
 // Delete goal
 const deleteGoal = async (req, res, next) => {
-  res.status(200).json({ msg: "Goal deleted!" });
+  const { id } = req.params;
+
+  const goal = await Goal.findById(id);
+
+  if (!goal) {
+    return res.status(404).json({
+      msg: `Goal with ID: ${id} not found!`,
+    });
+  }
+
+  await goal.remove();
+
+  res.status(200).json({ msg: "Goal deleted!", data: goal });
 };
 
 module.exports = {
