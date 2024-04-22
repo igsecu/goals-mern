9d3fe41a941cc7dc4b296c64df0f3883df89d35e
Ignacio Cunial <igsecu@hotmail.com>
2024-04-22 19:37:18 -0300

    Initialized express server

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,34 @@
-console.log("Hello World!");
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Body-Parser Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Res Headers
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+});
+
+// Error catching endware
+app.use((err, req, res, next) => {
+  const status = err.status || 500;
+  const message = err.message || err;
+  res.status(status).json({
+    statusCode: status,
+    msg: message,
+  });
+});
+
+// Initialized Express Server
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}...`);
+});
