b16f507457ce96b46afdb837ff1d74632292ddbe
Ignacio Cunial <igsecu@hotmail.com>
2024-04-27 18:43:37 -0300

    Returned mongo connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,16 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 
+let conn;
+
 const connectDB = async () => {
   try {
-    await mongoose.connect(
+    conn = await mongoose.connect(
       `mongodb://${process.env.DB_HOST}/${process.env.DB_NAME}`
     );
 
     console.log("Connected to MongoDB!");
+    return conn;
   } catch (error) {
     console.log(error);
     process.exit(1);
