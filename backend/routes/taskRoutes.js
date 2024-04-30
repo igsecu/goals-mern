f6d51454eaf40d9e5f489aad30f7f44866bd36bf
Ignacio Cunial <igsecu@hotmail.com>
2024-04-30 18:21:51 -0300

    Added PUT /:id/status/progress /completed routes

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -3,9 +3,17 @@ const router = express.Router();
 
 const { protect } = require("../middleware/authMiddleware");
 
-const { createTask } = require("../controllers/taskController");
+const {
+  createTask,
+  updateTaskCompleted,
+  updateTaskProgress,
+} = require("../controllers/taskController");
 
 // Create task
 router.post("/", protect, createTask);
+// Update task progress
+router.put("/:id/status/progress", protect, updateTaskProgress);
+// Update task completed
+router.put("/:id/status/completed", protect, updateTaskCompleted);
 
 module.exports = router;
