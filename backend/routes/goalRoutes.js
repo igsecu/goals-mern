e96a5fa0e0571efff28fd16258bbb7eb848b83a1
Ignacio Cunial <igsecu@hotmail.com>
2024-04-30 17:23:49 -0300

    Added /completed route

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -8,10 +8,13 @@ const {
   createGoal,
   updateGoal,
   deleteGoal,
+  getCompletedGoals,
 } = require("../controllers/goalController");
 
 const { protect } = require("../middleware/authMiddleware");
 
+// Get completed goals
+router.get("/completed", protect, getCompletedGoals);
 // Get high urgency goals
 router.get("/urgency/high", protect, getHighGoals);
 // Get medium urgency goals
