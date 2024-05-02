a01c80dd9a27aef7d829ceba9d3718a76eb28ab8
Ignacio Cunial <igsecu@hotmail.com>
2024-05-02 12:50:09 -0300

    Added alert messages

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,15 +1,21 @@
 import logo from "../images/goals.png";
-import { FaUser } from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
 import { RiLockPasswordFill } from "react-icons/ri";
-import { FaKey } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import Spinner from "../components/Spinner";
 import Navbar from "../components/Navbar";
 
 import { useState, useEffect } from "react";
 
+import { useNavigate } from "react-router-dom";
+
+import { GlobalContext } from "../context/GlobalState";
+import { useContext } from "react";
+
 const Login = () => {
+  const { register } = useContext(GlobalContext);
+  const navigate = useNavigate();
+
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
@@ -27,28 +33,47 @@ const Login = () => {
     };
 
     try {
-      const res = await fetch("http://localhost:5000/api/login", {
+      setLoading(true);
+      const res = await fetch("http://localhost:5000/api/users/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
         body: JSON.stringify(user),
       });
+
+      const data = await res.json();
+
+      if (data.statusCode === 200) {
+        localStorage.setItem(
+          "user",
+          JSON.stringify({ ...data.data, token: data.token })
+        );
+        register({ ...data.data, token: data.token });
+        navigate("/");
+      } else {
+        setMessage(data.msg);
+        setShow(true);
+        setLoading(false);
+      }
     } catch (error) {
       console.log(error);
     }
   };
 
   return (
-    <div className="vh-100 bg-black">
+    <div className="vh-100 bg-dark">
       <Navbar />
       <div className="h-75 d-flex justify-content-center align-items-center">
         {loading ? (
           <Spinner />
         ) : (
-          <div className="border border-2 rounded border-secondary bg-dark d-flex align-items-center py-5">
-            <div className="px-5">
-              <h1 className="text-white mb-5 text-center">Log in</h1>
+          <div
+            className="bg-dark d-flex align-items-center py-5"
+            style={{ maxWidth: "1000px" }}
+          >
+            <div className="px-0 px-md-5 w-100">
+              <h1 className="text-white mb-5 text-center">Login</h1>
               <form onSubmit={onSubmit}>
                 <div className="mb-3 d-flex align-items-center">
                   <MdEmail className="text-white me-3 fs-3" />
