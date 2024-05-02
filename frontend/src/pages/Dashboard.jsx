38a8b61ecd542b47600722074650ac58cda886b4
Ignacio Cunial <igsecu@hotmail.com>
2024-05-02 12:49:40 -0300

    Removed logout button

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,5 +1,25 @@
+import { GlobalContext } from "../context/GlobalState";
+import { useContext, useEffect } from "react";
+
+import { useNavigate } from "react-router-dom";
+
+import NavbarDashboard from "../components/NavbarDashboard";
+
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  const { user } = useContext(GlobalContext);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!user) {
+      navigate("/login");
+    }
+  }, [user, navigate]);
+
+  return (
+    <div>
+      <NavbarDashboard />
+    </div>
+  );
 };
 
 export default Dashboard;
