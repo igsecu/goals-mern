0dc8808d41c11d4b88be35c158fc97bca870040f
Ignacio Cunial <igsecu@hotmail.com>
2024-05-02 12:48:17 -0300

    Imported GlobalProvider

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,18 +3,24 @@ import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 
+import { GlobalProvider } from "./context/GlobalState";
+
+import ProtectedRoutes from "./utils/ProtectedRoutes";
+
 function App() {
   return (
     <>
-      <Router>
-        <div className="container">
+      <GlobalProvider>
+        <Router>
           <Routes>
             <Route path="/" element={<Dashboard />} />
-            <Route path="/login" element={<Login />} />{" "}
-            <Route path="/register" element={<Register />} />
+            <Route element={<ProtectedRoutes />}>
+              <Route path="/login" element={<Login />} />{" "}
+              <Route path="/register" element={<Register />} />
+            </Route>
           </Routes>
-        </div>
-      </Router>
+        </Router>
+      </GlobalProvider>
     </>
   );
 }
