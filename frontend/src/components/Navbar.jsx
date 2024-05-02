22efc506df53da411ac3968c9a8d6645a82d0e82
Ignacio Cunial <igsecu@hotmail.com>
2024-05-02 12:48:44 -0300

    Created Navbar component

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import logo from "../images/goals.png";
 
 const Navbar = () => {
   return (
-    <nav className="navbar p-3 text-white bg-black">
+    <nav className="navbar p-3 text-white bg-dark">
       <div className="d-flex flex-justify-start align-items-center">
         <img src={logo} alt="logo" style={{ width: "50px" }} />
         <h1 className="mb-0">Goals</h1>
