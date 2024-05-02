59b6f7ef3d78fbb2139b004fd132eca19627a727
Ignacio Cunial <igsecu@hotmail.com>
2024-05-02 12:50:20 -0300

    Added alert messages

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -5,10 +5,18 @@ import { RiLockPasswordFill } from "react-icons/ri";
 import { FaKey } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import Spinner from "../components/Spinner";
+import Navbar from "../components/Navbar";
 
-import { useState, useEffect } from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { GlobalContext } from "../context/GlobalState";
+import { useContext } from "react";
 
 const Register = () => {
+  const { register } = useContext(GlobalContext);
+  const navigate = useNavigate();
+
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -30,6 +38,7 @@ const Register = () => {
     };
 
     try {
+      setLoading(true);
       const res = await fetch("http://localhost:5000/api/users", {
         method: "POST",
         headers: {
@@ -37,90 +46,111 @@ const Register = () => {
         },
         body: JSON.stringify(newUser),
       });
+
+      const data = await res.json();
+
+      if (data.statusCode === 201) {
+        localStorage.setItem(
+          "user",
+          JSON.stringify({ ...data.data, token: data.token })
+        );
+        register({ ...data.data, token: data.token });
+        navigate("/");
+      } else {
+        setMessage(data.msg);
+        setShow(true);
+        setLoading(false);
+      }
     } catch (error) {
       console.log(error);
     }
   };
 
   return (
-    <div className="bg-black vh-100 d-flex justify-content-center align-items-center">
-      {loading ? (
-        <Spinner />
-      ) : (
-        <div className="border border-2 rounded border-secondary bg-dark d-flex align-items-center p-5 pe-lg-0">
-          <div>
-            <h1 className="text-white mb-5 text-center">Sign Up</h1>
-            <form onSubmit={onSubmit}>
-              <div className="mb-3 d-flex align-items-center">
-                <FaUser className="text-white me-3 fs-3" />
-                <input
-                  type="text"
-                  className="form-control bg-dark text-white"
-                  placeholder="Your Name"
-                  value={name}
-                  onChange={(e) => setName(e.target.value)}
-                />
-              </div>
-              <div className="mb-3 d-flex align-items-center">
-                <MdEmail className="text-white me-3 fs-3" />
-                <input
-                  type="email"
-                  className="form-control bg-dark text-white"
-                  placeholder="Your Email"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                />
-              </div>
-              <div className="mb-3 d-flex align-items-center">
-                <RiLockPasswordFill className="text-white me-3 fs-3 " />
-                <input
-                  type="password"
-                  className="form-control bg-dark text-white"
-                  placeholder="Your Password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                />
-              </div>
-              <div className="mb-3 d-flex align-items-center">
-                <FaKey className="text-white me-3 fs-3" />
-                <input
-                  type="password"
-                  className="form-control bg-dark text-white"
-                  placeholder="Repeat your password"
-                  value={password2}
-                  onChange={(e) => setPassword2(e.target.value)}
-                />
-              </div>
+    <div className="vh-100 bg-dark">
+      <Navbar />
+      <div className="h-75 d-flex justify-content-center align-items-center">
+        {loading ? (
+          <Spinner />
+        ) : (
+          <div
+            className="bg-dark d-flex align-items-center py-5"
+            style={{ maxWidth: "1000px" }}
+          >
+            <div className="px-0 px-md-5 w-100">
+              <h1 className="text-white mb-5 text-center">Sign Up</h1>
+              <form onSubmit={onSubmit}>
+                <div className="mb-3 d-flex align-items-center">
+                  <FaUser className="text-white me-3 fs-3" />
+                  <input
+                    type="text"
+                    className="form-control bg-dark text-white"
+                    placeholder="Your Name"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                  />
+                </div>
+                <div className="mb-3 d-flex align-items-center">
+                  <MdEmail className="text-white me-3 fs-3" />
+                  <input
+                    type="text"
+                    className="form-control bg-dark text-white"
+                    placeholder="Your Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </div>
+                <div className="mb-3 d-flex align-items-center">
+                  <RiLockPasswordFill className="text-white me-3 fs-3 " />
+                  <input
+                    type="password"
+                    className="form-control bg-dark text-white"
+                    placeholder="Your Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                </div>
+                <div className="mb-3 d-flex align-items-center">
+                  <FaKey className="text-white me-3 fs-3" />
+                  <input
+                    type="password"
+                    className="form-control bg-dark text-white"
+                    placeholder="Repeat your password"
+                    value={password2}
+                    onChange={(e) => setPassword2(e.target.value)}
+                  />
+                </div>
 
-              <div className="d-flex flex-column justify-content-center align-items-center mt-4">
-                {show ? (
-                  <div
-                    className="alert alert-danger text-center p-1 w-100"
-                    role="alert"
+                <div className="d-flex flex-column justify-content-center align-items-center mt-4">
+                  {show ? (
+                    <div
+                      className="alert alert-danger text-center p-1 w-100"
+                      role="alert"
+                    >
+                      {message}!
+                    </div>
+                  ) : (
+                    <></>
+                  )}
+                  <button type="submit" className="btn btn-success w-100">
+                    Register
+                  </button>
+
+                  <Link
+                    to="/login"
+                    className="text-decoration-none text-white mt-3 border-top border-1 w-100 pt-2 text-center"
                   >
-                    {message}!
-                  </div>
-                ) : (
-                  <></>
-                )}
-                <button type="submit" className="btn btn-success w-100">
-                  Register
-                </button>
-
-                <Link
-                  to="/login"
-                  className="text-decoration-none text-white mt-3 border-top border-1 w-100 pt-2 text-center"
-                >
-                  <small className="fw-bold">Already have an account?</small>
-                </Link>
-              </div>
-            </form>
+                    <small className="fw-bold">Already have an account?</small>
+                  </Link>
+                </div>
+              </form>
+            </div>
+            <div>
+              <img className="d-none d-lg-block" src={logo} alt="" />
+            </div>
           </div>
-          <div>
-            <img className="d-none d-lg-block" src={logo} alt="" />
-          </div>
-        </div>
-      )}
+        )}
+      </div>
     </div>
   );
 };
