584bc7be3cf0e5a9b8365adc0e9030cd6d4a97a8
Ignacio Cunial <igsecu@hotmail.com>
2024-05-03 17:54:54 -0300

    Added updateLow action

--- frontend/src/context/GlobalState.js
+++ frontend/src/context/GlobalState.js
@@ -6,6 +6,12 @@ const user = JSON.parse(localStorage.getItem("user"));
 
 const initialState = {
   user: user ? user : null,
+  loaded: false,
+  message: "",
+  low: [],
+  medium: [],
+  high: [],
+  completed: [],
 };
 
 // Create context
@@ -29,12 +35,65 @@ export const GlobalProvider = ({ children }) => {
     });
   };
 
+  const setLoaded = (loaded) => {
+    dispatch({
+      type: "LOADED",
+      payload: loaded,
+    });
+  };
+
+  const postLow = (low) => {
+    dispatch({
+      type: "POST_LOW",
+      payload: low,
+    });
+  };
+
+  const postMedium = (medium) => {
+    dispatch({
+      type: "POST_MEDIUM",
+      payload: medium,
+    });
+  };
+
+  const postHigh = (high) => {
+    dispatch({
+      type: "POST_HIGH",
+      payload: high,
+    });
+  };
+
+  const postCompleted = (completed) => {
+    dispatch({
+      type: "POST_COMPLETED",
+      payload: completed,
+    });
+  };
+
+  const updateLow = (goal) => {
+    dispatch({
+      type: "UPDATE_GOAL",
+      payload: goal,
+    });
+  };
+
   return (
     <GlobalContext.Provider
       value={{
         user: state.user,
+        low: state.low,
+        medium: state.medium,
+        high: state.high,
+        completed: state.completed,
+        loaded: state.loaded,
         register,
         logout,
+        setLoaded,
+        postLow,
+        postMedium,
+        postHigh,
+        postCompleted,
+        updateLow,
       }}
     >
       {children}
