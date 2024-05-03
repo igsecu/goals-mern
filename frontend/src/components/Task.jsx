6bb4efe89c8ae55029e25e5b54381da5ead48b19
Ignacio Cunial <igsecu@hotmail.com>
2024-05-03 18:49:21 -0300

    Added update task feature

--- frontend/src/components/Task.jsx
+++ frontend/src/components/Task.jsx
@@ -1,4 +1,5 @@
 import { MdDelete } from "react-icons/md";
+import { RxUpdate } from "react-icons/rx";
 
 const Task = ({ task, goal, action, user }) => {
   const deleteTask = async (id) => {
@@ -14,6 +15,47 @@ const Task = ({ task, goal, action, user }) => {
     if (data.statusCode === 200) {
       goal.tasks = goal.tasks.filter((t) => t._id !== id);
       action(goal);
+    }
+  };
+
+  const updateTask = async (id) => {
+    if (task.status === "NOT STARTED") {
+      const res = await fetch(
+        `http://localhost:5000/api/tasks/${id}/status/progress`,
+        {
+          method: "PUT",
+          headers: {
+            Authorization: `Bearer ${user.token}`,
+          },
+        }
+      );
+
+      const data = await res.json();
+
+      if (data.statusCode === 200) {
+        task.status = "IN PROGRESS";
+        goal.tasks = goal.tasks.map((t) => (t._id === id ? task : t));
+        action(goal);
+      }
+    }
+    if (task.status === "IN PROGRESS") {
+      const res = await fetch(
+        `http://localhost:5000/api/tasks/${id}/status/completed`,
+        {
+          method: "PUT",
+          headers: {
+            Authorization: `Bearer ${user.token}`,
+          },
+        }
+      );
+
+      const data = await res.json();
+
+      if (data.statusCode === 200) {
+        task.status = "COMPLETED";
+        goal.tasks = goal.tasks.map((t) => (t._id === id ? task : t));
+        action(goal);
+      }
     }
   };
 
@@ -37,6 +79,14 @@ const Task = ({ task, goal, action, user }) => {
       </p>
 
       <div className="d-flex justify-content-end align-items-center mb-2">
+        {task.status !== "COMPLETED" && (
+          <RxUpdate
+            className="text-dark fs-5 me-2"
+            type="button"
+            onClick={() => updateTask(task._id)}
+          />
+        )}
+
         <MdDelete
           className="text-dark fs-4 me-2"
           type="button"
