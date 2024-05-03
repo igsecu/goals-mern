93ea4a85e30eff983bedd2644a9e0b092564bf51
Ignacio Cunial <igsecu@hotmail.com>
2024-05-03 17:54:39 -0300

    Added updateLow action

--- frontend/src/context/AppReducer.js
+++ frontend/src/context/AppReducer.js
@@ -12,6 +12,44 @@ export const reducer = (state, action) => {
         user: null,
       };
     }
+    case "LOADED": {
+      return {
+        ...state,
+        loaded: action.payload,
+      };
+    }
+    case "POST_LOW": {
+      return {
+        ...state,
+        low: action.payload,
+      };
+    }
+    case "POST_MEDIUM": {
+      return {
+        ...state,
+        medium: action.payload,
+      };
+    }
+    case "POST_HIGH": {
+      return {
+        ...state,
+        high: action.payload,
+      };
+    }
+    case "POST_COMPLETED": {
+      return {
+        ...state,
+        completed: action.payload,
+      };
+    }
+    case "UPDATE_GOAL": {
+      return {
+        ...state,
+        low: state.low.map((l) =>
+          l._id === action.payload._id ? l : action.payload
+        ),
+      };
+    }
     default:
       return state;
   }
