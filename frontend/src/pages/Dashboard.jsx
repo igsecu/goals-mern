8fa1d69e8f24b392d723de8590031ed20cffb04f
Ignacio Cunial <igsecu@hotmail.com>
2024-05-06 14:43:25 -0300

    Added progress bar functionality

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -29,6 +29,7 @@ const Dashboard = () => {
   const navigate = useNavigate();
 
   const [loading, setLoading] = useState(false);
+  const [process, setProcess] = useState(0);
 
   useEffect(() => {
     const fetchLow = async () => {
@@ -123,15 +124,31 @@ const Dashboard = () => {
     }
   }, [user, navigate]);
 
+  useEffect(() => {
+    const totalGoals =
+      low?.length + medium?.length + high?.length + completed?.length;
+
+    setProcess(((completed?.length * 100) / totalGoals).toFixed(0));
+  }, [low, medium, high, completed]);
+
   return (
-    <div style={{ height: "750px" }}>
+    <div style={{ minHeight: "750px" }}>
       <NavbarDashboard />
-      <div className="d-flex justify-content-end pe-5 py-3 bg-dark">
+      <div className="d-flex flex-column flex-lg-row justify-content-between align-items-center px-5 py-3 bg-dark">
+        <div
+          className="progress w-75 mb-3 mb-lg-0"
+          role="progressbar"
+          aria-label="Example with label"
+        >
+          <div className="progress-bar" style={{ width: `${process}%` }}>
+            {process}%
+          </div>
+        </div>
         <Link
           to="/create-goal"
           className="d-flex align-items-center btn btn-success"
         >
-          <FaPlus className="me-2" /> <span>CREATE NEW GOAL</span>
+          <FaPlus className="me-2" /> <span>NEW GOAL</span>
         </Link>
       </div>
       {loading ? (
@@ -139,28 +156,40 @@ const Dashboard = () => {
       ) : (
         <div className="container-fluid bg-dark h-100 p-4">
           <div className="row h-100">
-            <div className="col-lg-3 overflow-scroll h-100 d-flex flex-column align-items-center">
+            <div
+              className="col-lg-3 overflow-scroll d-flex flex-column align-items-center"
+              style={{ maxHeight: "650px" }}
+            >
               {typeof low === "string" ? (
                 <p className="text-white">{low}</p>
               ) : (
                 low?.map((g) => <LowGoal key={g._id} goal={g} />)
               )}
             </div>
-            <div className="d-none d-lg-block col-lg-3 overflow-scroll h-100 d-flex flex-column align-items-center">
+            <div
+              className="col-lg-3 overflow-scroll h-100 d-flex flex-column align-items-center"
+              style={{ maxHeight: "650px" }}
+            >
               {typeof medium === "string" ? (
                 <p className="text-white">{medium}</p>
               ) : (
                 medium?.map((g) => <MediumGoal key={g._id} goal={g} />)
               )}
             </div>
-            <div className="d-none d-lg-block col-lg-3 overflow-scroll h-100 d-flex flex-column align-items-center">
+            <div
+              className="col-lg-3 overflow-scroll h-100 d-flex flex-column align-items-center"
+              style={{ maxHeight: "650px" }}
+            >
               {typeof high === "string" ? (
                 <p className="text-white">{high}</p>
               ) : (
                 high?.map((g) => <HighGoal key={g._id} goal={g} />)
               )}
             </div>
-            <div className="d-none d-lg-block col-lg-3 overflow-scroll h-100 d-flex flex-column align-items-center">
+            <div
+              className="col-lg-3 overflow-scroll h-100 d-flex flex-column align-items-center"
+              style={{ maxHeight: "650px" }}
+            >
               {typeof completed === "string" ? (
                 <p className="text-white">{completed}</p>
               ) : (
