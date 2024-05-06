ec833cd59947f1810da70958c90d52074a729b81
Ignacio Cunial <igsecu@hotmail.com>
2024-05-06 18:58:50 -0300

    Removed profile li

--- frontend/src/components/NavbarDashboard.jsx
+++ frontend/src/components/NavbarDashboard.jsx
@@ -5,13 +5,15 @@ import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
 
 import logo from "../images/goals.png";
-import { FaCircleUser } from "react-icons/fa6";
-import { FaCircle } from "react-icons/fa";
-import { ImProfile } from "react-icons/im";
+import { FaCircle, FaCheck, FaArrowRight } from "react-icons/fa";
 import { MdLogout } from "react-icons/md";
-import { Link } from "react-router-dom";
 
-const NavbarDashboard = () => {
+const NavbarDashboard = ({
+  setShowLow,
+  setShowMedium,
+  setShowHigh,
+  setShowCompleted,
+}) => {
   const {
     logout,
     user,
@@ -34,6 +36,37 @@ const NavbarDashboard = () => {
     navigate("/login");
   };
 
+  const showLowGoals = () => {
+    setShowLow(true);
+    setShowMedium(false);
+    setShowHigh(false);
+    setShowCompleted(false);
+  };
+  const showMediumGoals = () => {
+    setShowLow(false);
+    setShowMedium(true);
+    setShowHigh(false);
+    setShowCompleted(false);
+  };
+  const showHighGoals = () => {
+    setShowLow(false);
+    setShowMedium(false);
+    setShowHigh(true);
+    setShowCompleted(false);
+  };
+  const showCompletedGoals = () => {
+    setShowLow(false);
+    setShowMedium(false);
+    setShowHigh(false);
+    setShowCompleted(true);
+  };
+  const showAll = () => {
+    setShowLow(true);
+    setShowMedium(true);
+    setShowHigh(true);
+    setShowCompleted(true);
+  };
+
   return (
     <nav className="navbar navbar-expand-lg bg-dark navbar-dark py-3 text-white ">
       <div className="container-fluid">
@@ -53,67 +86,68 @@ const NavbarDashboard = () => {
 
         <div className="collapse navbar-collapse " id="navmenu">
           <ul className="navbar-nav ms-auto">
-            <div className="d-block d-md-none border-bottom border-1 p-2 mb-2">
+            <div className="d-block d-lg-none border-bottom border-1 p-2 mb-2">
               <li className="nav-item">
-                <Link
+                <div
                   className="nav-link text-white d-flex justify-content-center align-items-center"
-                  to=""
+                  onClick={showLowGoals}
+                  style={{ cursor: "pointer" }}
                 >
                   <FaCircle className="text-success me-2" />
                   <p className="mb-0">Low Urgency Goals</p>
-                </Link>
+                </div>
               </li>
               <li className="nav-item">
-                <Link
+                <div
                   className="nav-link text-white d-flex justify-content-center align-items-center"
-                  to=""
+                  onClick={showMediumGoals}
+                  style={{ cursor: "pointer" }}
                 >
                   <FaCircle className="text-warning me-2" />
                   <p className="mb-0">Medium Urgency Goals</p>
-                </Link>
+                </div>
               </li>
               <li className="nav-item">
-                <Link
+                <div
                   className="nav-link text-white d-flex justify-content-center align-items-center"
-                  to=""
+                  onClick={showHighGoals}
+                  style={{ cursor: "pointer" }}
                 >
                   <FaCircle className="text-danger me-2" />
                   <p className="mb-0">High Urgency Goals</p>
-                </Link>
+                </div>
+              </li>
+              <li className="nav-item">
+                <div
+                  className="nav-link text-white d-flex justify-content-center align-items-center"
+                  onClick={showCompletedGoals}
+                  style={{ cursor: "pointer" }}
+                >
+                  <FaCheck className="text-primary me-2" />
+                  <p className="mb-0">Completed Goals</p>
+                </div>
+              </li>
+              <li className="nav-item">
+                <div
+                  className="nav-link text-white d-flex justify-content-center align-items-center"
+                  onClick={showAll}
+                  style={{ cursor: "pointer" }}
+                >
+                  <FaArrowRight className="text-white me-2" />
+                  <p className="mb-0">All Goals</p>
+                </div>
               </li>
             </div>
 
-            <div className="d-none d-md-block d-flex flex-column align-items-center p-2 justify-content-center dropdown">
+            <div className="d-none d-lg-block d-flex flex-column align-items-center p-2 justify-content-center dropdown">
               <div
                 className=" d-flex align-items-center"
                 data-bs-toggle="dropdown"
                 style={{ cursor: "pointer" }}
               >
-                {user && user.image ? (
-                  <img
-                    className="rounded-circle"
-                    src={user.image}
-                    alt="user"
-                    style={{ width: "25px", height: "25px" }}
-                  />
-                ) : (
-                  <FaCircleUser
-                    style={{ width: "25px", height: "25px" }}
-                    className="me-2"
-                  />
-                )}
-                <h5 className="mb-0 ">{user?.name}</h5>
+                <h5 className="mb-0 ">Hello, {user?.name}!</h5>
               </div>
               <ul className="dropdown-menu border-0 bg-dark rounded-0">
-                <li>
-                  <Link
-                    className="text-white text-decoration-none d-flex align-items-center ps-3 mb-3"
-                    to=""
-                  >
-                    <ImProfile />
-                    <p className="mb-0 ms-2">Profile</p>
-                  </Link>
-                </li>
                 <li>
                   <div
                     className="text-white d-flex align-items-center ps-3 pb-2"
@@ -127,38 +161,17 @@ const NavbarDashboard = () => {
               </ul>
             </div>
 
-            <div className="d-block d-md-none d-flex flex-column align-items-center p-2 justify-content-center">
+            <div className="d-block d-lg-none d-flex flex-column align-items-center p-2 justify-content-center">
               <div
                 className="d-flex align-items-center"
                 data-bs-toggle="dropdown"
                 style={{ cursor: "pointer" }}
               >
-                {user && user.image ? (
-                  <img
-                    className="rounded-circle"
-                    src={user.image}
-                    alt="user"
-                    style={{ width: "20px", height: "20px" }}
-                  />
-                ) : (
-                  <FaCircleUser
-                    style={{ width: "20px", height: "20px" }}
-                    className="me-2"
-                  />
-                )}
-                <h5 className="mb-0 ">{user?.name}</h5>
+                <h5 className="mb-0 ">Hello, {user?.name}!</h5>
               </div>
 
-              <Link
-                className="text-white text-decoration-none d-flex justify-content-start align-items-center my-3"
-                to=""
-              >
-                <ImProfile />
-                <p className="mb-0 ms-2">Profile</p>
-              </Link>
-
               <div
-                className="text-white d-flex justify-content-center align-items-center"
+                className="mt-3 text-white d-flex justify-content-center align-items-center"
                 style={{ cursor: "pointer" }}
                 onClick={logoutUser}
               >
