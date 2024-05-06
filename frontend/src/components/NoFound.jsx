6efad0730989509fd7831ba72abe5fcb6fa26021
Ignacio Cunial <igsecu@hotmail.com>
2024-05-06 18:34:11 -0300

    Created NoFound component

---
+++ frontend/src/components/NoFound.jsx
@@ -0,0 +1,23 @@
+import { FaExclamation, FaPlus } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+const NoFound = ({ text }) => {
+  return (
+    <div
+      className="text-center bg-dark d-flex flex-column align-items-center justify-content-center"
+      style={{ height: "750px" }}
+    >
+      <FaExclamation className="text-danger fs-1 mb-1" />
+      <h5 className="text-white">You do not have goals yet!</h5>
+      <p className="fw-bold">{text}</p>
+      <Link
+        to="/create-goal"
+        className="d-flex align-items-center btn btn-success"
+      >
+        <FaPlus className="me-2" /> <span>NEW GOAL</span>
+      </Link>
+    </div>
+  );
+};
+
+export default NoFound;
