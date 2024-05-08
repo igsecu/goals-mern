d26d744a41718cb49e00850183a7df3148c25cc3
Ignacio Cunial <igsecu@hotmail.com>
2024-05-08 14:17:03 -0300

    Updated logo and title styles

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,8 +4,10 @@ const Navbar = () => {
   return (
     <nav className="navbar p-3 text-white bg-dark">
       <div className="d-flex flex-justify-start align-items-center">
-        <img src={logo} alt="logo" style={{ width: "50px" }} />
-        <h1 className="mb-0">Goals</h1>
+        <img src={logo} alt="logo" style={{ width: "40px" }} />
+        <h2 className="mb-0">
+          Goals<span className="text-danger">Manager</span>
+        </h2>
       </div>
     </nav>
   );
